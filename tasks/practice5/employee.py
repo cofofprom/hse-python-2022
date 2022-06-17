7c9b68e6bcb1426b9322c07b2c4b8b53674a924e
cofofprom <nightmarelandscape@gmail.com>
2022-06-17 22:12:39 +0300

    fifth task solved

--- tasks/practice5/employee.py
+++ tasks/practice5/employee.py
@@ -38,14 +38,16 @@ class Employee:
         Задача: реализовать конструктор класса, чтобы все тесты проходили
         """
 
-        # пиши свой код здесь
+        self.name = name
+        self.position = position
+        self._salary = int(salary)
 
     def get_salary(self) -> int:
         """
         Метод возвращает зарплату сотрудника.
         """
 
-        # пиши свой код здесь
+        return self._salary
 
     def __eq__(self, other: object) -> bool:
         """
@@ -54,8 +56,12 @@ class Employee:
         Сравнение происходит по уровню позиции см. `get_position_level`.
         Если что-то идет не так - бросаются исключения. Смотрим что происходит в тестах.
         """
-
-        # пиши свой код здесь
+        if not isinstance(other, Employee): raise TypeError
+        try:
+            res = get_position_level(self.position) == get_position_level(other.position)
+        except NoSuchPositionError:
+            raise ValueError
+        return res
 
     def __str__(self):
         """
@@ -63,7 +69,7 @@ class Employee:
         Пример вывода: 'name: Ivan position manager'
         """
 
-        # пиши свой код здесь
+        return f'name: {self.name} position: {self.position}'
 
     def __hash__(self):
         return id(self)
@@ -81,8 +87,8 @@ class Developer(Employee):
         """
         Задача: реализовать конструктор класса, используя конструктор родителя
         """
-
-        # пиши свой код здесь
+        super(Developer, self).__init__(name, self.position, salary)
+        self.language = language
 
 
 class Manager(Employee):
@@ -96,5 +102,5 @@ class Manager(Employee):
         """
         Задача: реализовать конструктор класса, используя конструктор родителя
         """
-
+        super(Manager, self).__init__(name, self.position, salary)
         # пиши свой код здесь
